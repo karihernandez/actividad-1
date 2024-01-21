87a66a1c474170794729ef9cd8e7579bd2f2475c
Kari Hernandez <anntonellakari@gmail.com>
2024-01-21 09:53:50 +0000

    Primer commit, inicio del proyecto

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var myButton = document.getElementById("myButton");
+
+  myButton.addEventListener("click", function () {
+    alert("¡Haz hecho clic en el botón!");
+  });
+});
