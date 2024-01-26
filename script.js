ce686a77134ba1e6c5edc32371c2d03b549afb6b
Kari Hernandez <anntonellakari@gmail.com>
2024-01-26 13:08:06 +0000

    Creación de la rama feature-1 y añadir la clase A con los atributos: foo bar

--- script.js
+++ script.js
@@ -1,7 +1,44 @@
+class ClaseA {
+  constructor() {
+    this.foo = 0;
+    this.bar = 0;
+
+    this.fooInc = 1;
+    this.barInc = 2;
+  }
+
+  tick() {
+    this.foo += this.fooInc;
+    this.bar += this.barInc;
+  }
+
+  inc() {
+    this.fooInc *= 2;
+    this.barInc *= 2;
+  }
+
+  print() {
+    return `
+Foo: ${this.foo}
+Bar: ${this.bar}
+FooInc: ${this.fooInc}
+BarInc: ${this.barInc}`.trim();
+  }
+}
+
 document.addEventListener("DOMContentLoaded", function () {
+  const objetoA = new ClaseA();
+
   var myButton = document.getElementById("myButton");
 
+  var counter = document.getElementById("counter");
+
+  setInterval(() => {
+    objetoA.tick();
+    counter.innerText = objetoA.print();
+  }, 250);
+
   myButton.addEventListener("click", function () {
-    alert("¡Haz hecho clic en el botón!");
+    objetoA.inc();
   });
 });
